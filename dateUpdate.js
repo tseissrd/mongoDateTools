5597d2d4a68b3b0149d7c72b45287fef2f9cb096
KPavlov <astartes@live.ru>
2020-02-07 15:54:57 +1000

    'update'

---
+++ dateUpdate.js
@@ -0,0 +1,23 @@
+downFrom = new Date(2020,0,1)
+upTo = new Date(2020,5,5)
+matchHours = 14
+hours = 10
+
+db.getCollection('khv-ticket-discount_ticket').update(
+    { $and: [
+    { dateAirGo: {$lte: upTo} },
+    { dateAirGo: {$gte: downFrom} }
+        ]
+},
+    [
+    { $set: { 'dateAirGo': { $switch: {
+                           branches: [
+                               { case: { $eq: [ {$hour: '$dateAirGo'}, matchHours ] }, then: {$toDate: {$sum: [{$toLong: '$dateAirGo'},1000*60*60*hours] } } },
+                               { case: { $ne: [ {$hour: '$dateAirGo'}, matchHours ] }, then: '$dateAirGo' },
+                           ],
+                           default: '$dateAirGo'
+     } } }}
+    //{ $set: { 'dateAirGo': {$toDate: {$sum: [{$toLong: '$dateAirGo'},1000*60*60*hours] } } }}
+    ],
+    { multi: true }
+)
---
+++ getUTCdates.js
@@ -0,0 +1,28 @@
+matchHours = 14
+
+db.getCollection('khv-ticket-discount_ticket').aggregate([
+    {
+        $project: {
+            hours: {$hour: '$dateAirGo'},
+            dateAirGo: 1
+        }
+    },
+    {
+        $match: {
+            hours: matchHours
+        }
+    }
+])
+    db.getCollection('khv-ticket-discount_ticket').aggregate([
+    {
+        $project: {
+            hours: {$hour: '$dateAirGo'},
+            dateAirGo: 1
+        }
+    },
+    {
+        $match: {
+            hours: {$ne: matchHours}
+        }
+    }
+])
